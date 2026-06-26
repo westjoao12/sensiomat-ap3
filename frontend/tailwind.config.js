3b0f18ff051459295edf64bd717880428821b559
westjoao12 <westjoao12@gmail.com>
2026-06-25 22:24:30 -0300

    feat: configurações do ficheiros do TailwindCSS e packageJson

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        darkBg: '#0B0F19',
+        darkSurface: '#151C2C',
+        brandAccent: '#38BDF8',
+      }
+    },
+  },
+  plugins: [],
+}
